96057531bdb3eba5c3a014f69be71961c5ca2f36
mohammad khajehnoori <mohammad.khajehnoori.05@gmail.com>
2024-10-18 15:26:48 +0330

    some other functions were added .

--- S_1_solution/src/s_1_solution/S_1_Solution.java
+++ S_1_solution/src/s_1_solution/S_1_Solution.java
@@ -1,5 +1,8 @@
 package s_1_solution;
  
+import java.util.ArrayList;
+
+
 public class S_1_Solution {
     
     
@@ -163,44 +166,117 @@ public class S_1_Solution {
     }
     
     
-//    =================== Q8 ==================
-//    =============== Truth Table =============
-    
+//    =================== Q8 ====================
+//    =============== Truth Table ===============
+    static int times = 1; 
+    static int name = 97;
     public static void TruthTable(int n){
-        if( n == 0 ){
-            System.out.println("input must be greater than 0!");
+        if(n == 0){
+            return;
+        }
+        System.out.print((char)(name) + " -> ");
+        ++name;
+         double num = Math.pow(2 , n);
+         for(int i = 0 ; i < times ; ++i){
+             for(int j = 0 ; j< num ; ++j){
+                 if( j < num/2){
+                     System.out.print("0 ");
+                 }
+                 else{
+                     System.out.print("1 ");
+                 }
+             }
+         }
+         System.out.println();
+         times *= 2;
+         TruthTable(n-1);
+         
+    }
+//    =================== Q9 ==========================
+//    =============== the sum of factorials ===========
+    
+    public static int combo(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            int temp = 1;
+            for (int i = 2; i <= n; i++) {
+                temp *= i;
+            }
+
+            return temp + combo(n - 1);
+        }
+    } 
+    
+//    ===================== Q10 =========================
+//    =============== the sum of factorials 2 ===========
+    
+    public static double combo2(double n){
+        if(n == 1.0){
+            return 1;
         }
         
         else{
-            for(double i = 1 ; i <= Math.pow(2.0, n) ; i++){
-                for(int j = n ; j >= 1 ; j--){
-                    if( i <= (Math.pow(2.0,j)/2.0)){
-                        System.out.print("0 ");
-                    }
-                    else{
-                        System.out.print("1 ");
-                    }
-                }
-                System.out.println();
+            double temp = 1.0;
+            for(double i = 1.0 ; i <= n ; i++){
+                temp *= i;
             }
+            return (1.0/temp) + combo2( n - 1.0);
         }
+        
     }
     
-    
-//    =================== Q11 ==================
+//    =================== Q11 =======================
 //    =============== 2 - 5 - 10 rials ==============
     
-    public static void distribute(int n){
-        if(n < 2){
-            System.out.println("input must be greater than 1!");            
-        }
-        
-        else{
-            
-        }
+    public static void distribute(int n  , String res){
+         int coins [] = {2,5,10} ;
+         if(n == 0){
+             System.out.println(res);
+             return;
+         }
+         // n < 0 is invalid so ... we'll skip it.
+         if(n < 0 ){
+             return;
+         }
+         else{
+             for(int i = 0 ; i < 3 ; i++){
+                 distribute(n-coins[i]  ,(res + coins[i] + " "));
+             }
+         }
     }
     
-//    =================== Q12 ==================
-//    =============== Truth Table ==============
+//    ====================== Q12 ================================ 
+//    ======= 2 - 5 - 10 rials but a better version =============
     
+         public static void distribute2(){
+             
+         }
+    
+//    =================== Q13 =====================
+//    ================== hanoi ====================
+    public static void hanoi(int n , char s , char d , char a){
+ 
+    }
+    
+//    =================== Q16 =====================
+//    =============== zir majmooee ================
+    public static void SubSet(int [] array , int n){
+    }
+      
+//    =================== Q20 =====================
+//    =================== Ack =====================
+
+    public static int Ack(int m , int n){
+        if(n < 0 || m < 0){ 
+            return 0; 
+        }
+        else if( n == 0 ) return n+1;
+        else if( m == 0 ) return Ack(m-1,1);
+        else return Ack(m-1 , Ack(m , n-1));
+    }
+        
+    
+
+
 }
--- S_1_solution/src/s_1_solution/S_1_solution_main.java
+++ S_1_solution/src/s_1_solution/S_1_solution_main.java
@@ -4,34 +4,77 @@ public class S_1_solution_main {
 
    
     public static void main(String[] args) {
-       int array [] = {1 , 2 , 3 , 4 , 5};
+       int array [] = {1 , 2 , 3 , 4 , 5, 15};
         
 // --------- Q1_1 ---------------------------------------
 
+//    int res1 = S_1_Solution.kharej_ghesmat(10, 3);
+//    System.out.println(res1);
+
 // --------- Q2_1 ---------------------------------------
 
+//    int res2 = S_1_Solution.avg(array, 6);
+//    System.out.println(res2);
+
 // --------- Q3_1 ---------------------------------------
-    System.out.print("array : ");
-    for(int x = 0 ; x < array.length ; x++){
-        System.out.print(array[x] + "  ");
-    }
-    System.out.println();
-    int array_2 [] = S_1_Solution.reverse(array, 5);
-    System.out.print("reversed array : ");
-    for(int x = 0 ; x < array_2.length ; x++){
-        System.out.print(array_2[x] + "  ");
-    }
-    System.out.println();
+
+//    System.out.print("array : ");
+//    for(int x = 0 ; x < array.length ; x++){
+//        System.out.print(array[x] + "  ");
+//    }
+//    System.out.println();
+//    int array_2 [] = S_1_Solution.reverse(array, 5);
+//    System.out.print("reversed array : ");
+//    for(int x = 0 ; x < array_2.length ; x++){
+//        System.out.print(array_2[x] + "  ");
+//    }
+//    System.out.println();
 
 // --------- Q4_1 ---------------------------------------
 
-
+//    int res3 = S_1_Solution.binary(15);
+//    System.out.println(res3);
 
 // --------- Q5_1 ---------------------------------------
 
+//        int res4 = S_1_Solution.maximum(array, 6);
+//        System.out.println(res4);
+
+// --------- Q6_1 ---------------------------------------
+
+//        int res5 = S_1_Solution.product(4,10);
+//        System.out.println(res5);
+
+// --------- Q7_1 ---------------------------------------
+
+//        int res6 = S_1_Solution.bmm(18, 45);
+//        System.out.println(res6);
 
 // --------- Q8_1 ---------------------------------------
-        S_1_Solution.TruthTable(3);
+
+//        S_1_Solution.TruthTable(4); 
+        
+// --------- Q9_1 ---------------------------------------
+
+//        int a2 = S_1_Solution.combo(4);
+//        System.out.println(a2);
+            
+// --------- Q10_1 ---------------------------------------
+
+//        double a1 = S_1_Solution.combo2(4);
+//        System.out.println(a1);
+
+// --------- Q11_1 ---------------------------------------
+
+        S_1_Solution.distribute(20 , "");
+        
+// --------- Q12_1 ---------------------------------------
+
+// --------- Q20_1 ---------------------------------------
+
+//    System.out.println("Ack( 3 , 2 ) = "+S_1_Solution.Ack(3,2));
+
+         
         
     }
     }
